666fa5b2521f285bef2b272f0480b383a28c386f
simmk15 <sknagra15@gmail.com>
2025-08-10 20:41:42 +0530

    Dictionary

---
+++ Get_the_country_code.py
@@ -0,0 +1,5 @@
+country_code={'India' : '0091', 'Australia' : '0025', 'Nepal' : '00977'}
+print("country code for india-")
+print(country_code.get('India','Not found'))
+print("country code for japan-")
+print(country_code.get('Japan','Not found'))
